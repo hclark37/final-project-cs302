2c09f7d9f31c02f9ad03e94c3f00b1b7a5520476
hclark37 <hclark37@vols.utk.edu>
2025-03-31 13:26:26 -0400

    Add files via upload

---
+++ client.py
@@ -0,0 +1,20 @@
+import socket
+
+# define server address and port
+host = '127.0.0.1' 
+port = 8080  
+
+# create socket 
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# connect to server
+client_socket.connect((host, port))
+
+# message to send
+message = "Test message."
+
+# send the message
+client_socket.send(message.encode('utf-8'))
+
+# close the connection
+client_socket.close()
