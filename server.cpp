b2fc96703ec50eb4fea0a39ab82f1e2be0ac3f57
hclark37 <hclark37@vols.utk.edu>
2025-04-21 13:27:31 -0400

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -1,40 +1,202 @@
-// C++ program to show the example of server application in
-// socket programming
-// https://www.linuxhowtos.org/C_C++/socket.htm
-#include <cstring>
 #include <iostream>
-#include <netinet/in.h>
-#include <sys/socket.h>
+#include <string>
+#include <vector>
+#include <ctime>
+#include <cstring>
+#include <cstdlib>
 #include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <csignal> // for clean killing it with ctrl + c
+
+// https://www.linuxhowtos.org/C_C++/socket.htm
 
 using namespace std;
 
-int main() {
-    // creating socket
-    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+const int max_packets = 100;
+const int max_users = 100;
+int total_packets_added = 0;
 
-    // specifying the address
-    sockaddr_in serverAddress;
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(8080);
-    serverAddress.sin_addr.s_addr = INADDR_ANY;
+struct user_info {
+	string username;
+	time_t last_received_time = 0;
+};
+
+struct packet {
+	string user;
+	string message;
+	time_t receive_time;
+};
+
+vector<user_info> users;
+vector<packet> packets;
+packet intermediate_packet;
+
+void split_string(const string& received, string &username, string& message) {
+	for (size_t i = 0; i < received.length(); i++) {
+		if (received[i] == ':') {
+			username = received.substr(0, i);
+			message = received.substr(i + 1);
+			return;
+		}
+	}
 	
-    // binding socket
-    bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
-
-    // listening to the assigned socket
-    listen(serverSocket, 5);
-
-    // accepting connection request
-    int clientSocket = accept(serverSocket, nullptr, nullptr);
-
-    // recieving data
-    char buffer[1024] = { 0 };
-    recv(clientSocket, buffer, sizeof(buffer), 0);
-    cout << "Message from client: " << buffer << endl;
-
-    // closing the socket
-    close(serverSocket);
+	message = received; // fallback 
 	
-    return 0;
+	cerr << "ERROR: invalid message format!" << endl;
+}
+
+user_info* get_user(const string& username) {
+	for (int i = 0; i < users.size(); i++) {
+		if (users[i].username == username) {
+			return &users[i];
+		}
+	}
+	
+	if (users.size() < max_users) {
+		users.push_back({username, 0}); // should this set the time value?
+		return &users.back();
+	}
+	
+	return nullptr;
+}
+
+packet* add_packet(const string& msg) {
+	if (total_packets_added >= max_packets) {
+		//PARKER, you can add a function call here to push everything to a json file.
+		//not sure if this conditional statement is right
+		total_packets_added = 0;
+	}
+	
+	string username, message; 
+	time_t receive_time = time(NULL);
+	
+	split_string(msg, username, message);
+	
+	if (message.empty()) {
+		//cerr << "ERROR: message invalid!" << endl; //intended behavior since updates are with blank messages
+		intermediate_packet = {username, msg, receive_time}; //this is the worst possible way to do this. 
+		return &intermediate_packet;
+	}
+	
+	if (packets.size() == max_packets) {
+		packets.erase(packets.begin());
+	}
+	
+	packet new_packet = {username, message, receive_time};
+	
+	packets.push_back(new_packet);
+	
+	total_packets_added += 1; //iterator to see when you've fully looped through the vector
+	
+	return &packets.back();
+}
+
+int sock;
+
+//https://www.tutorialspoint.com/cplusplus/cpp_signal_handling.htm 
+void signal_handler(int signum) {
+	cout << endl << "Received SIGINT (Ctrl + C). Saving messages..." << endl;
+	
+	//parker, this is another place where you're gonna run your save to json part. 
+	//you're only going to want to save the ones since the last save in the program. you'll have to do the math on that
+	//based upon the total_packets_added part of the code
+	
+	close(sock);
+	
+	exit(0);
+}
+
+int main(int argc, char *argv[]) {
+	signal(SIGINT, signal_handler);
+
+	if (argc < 2) {
+		cerr << "ERROR: no port provided!" << endl;
+		return 1;
+	}
+	
+	sock = socket(AF_INET, SOCK_DGRAM, 0);
+	
+	if (sock < 0) { 
+		cerr << "ERROR: can't open socket!" << endl;
+		return 1;
+	}
+	
+	struct sockaddr_in server;
+	
+	server.sin_family = AF_INET;
+
+	server.sin_addr.s_addr = INADDR_ANY;
+
+	server.sin_port = htons(stoi(argv[1]));
+	
+	if (bind(sock, (struct sockaddr *)&server, sizeof(server)) < 0) {
+		cerr << "ERROR: couldn't bind!" << endl;
+		return 1;
+	}
+	
+	struct sockaddr_in from;
+
+	socklen_t fromlen = sizeof(from);
+	
+	char buffer[1024];
+
+	const string end_msg = "END";
+
+	while (true) {
+				
+		int n = recvfrom(sock, buffer, sizeof(buffer) - 1, 0, (struct sockaddr *)&from, &fromlen);
+
+		if (n < 0) {
+			cerr << "ERROR: problem receiving from socket!" << endl;
+			continue; //how to handle this? 
+		}
+		
+		buffer[n] = '\0'; //null terminate string 
+		
+        string received = buffer;
+		
+		packet* post = add_packet(received);
+		
+		user_info* user = get_user(post->user);
+		
+		cout << "Datagram received: " << received << endl;
+		
+		if (user == nullptr) { //send all if undefined user 
+			//cout << "User is nullptr!" << endl; // debug 
+			for (int i = 0; i < packets.size(); i++) {
+				int sent = sendto(sock, packets[i].message.c_str(), packets[i].message.size(), 0, (struct sockaddr *)&from, fromlen);
+				if (sent < 0) { 
+					cerr << "ERROR: can't send packet!" << endl;
+				}
+			}
+		} else { //only send those that are new since last message or 
+			for (int i = 0; i < packets.size(); i++) {
+				if (user->last_received_time <= packets[i].receive_time && user->username != packets[i].user) {
+					string message = packets[i].user + ":" + packets[i].message;
+					int sent = sendto(sock, message.c_str(), message.size(), 0, (struct sockaddr *)&from, fromlen);
+					if (sent < 0) { 
+						cerr << "ERROR: can't send packet!" << endl;
+					}
+				}
+			}
+		}
+		
+		if (user == nullptr) {
+			
+		} else {
+			user->last_received_time = post->receive_time;
+		}
+		
+		int sent = sendto(sock, end_msg.c_str(), end_msg.size(), 0, (struct sockaddr *)&from, fromlen);
+		
+		if (sent < 0) {
+			cerr << "ERROR: can't send END message!" << endl;
+		}		
+	}
+	
+	close(sock);
+	
+	return 0;
 }
