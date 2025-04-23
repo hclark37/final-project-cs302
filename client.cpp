6771c958e1d58d3d45e0d5682fd4a2fcef828c36
hclark37 <hclark37@vols.utk.edu>
2025-04-22 20:11:17 -0400

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -8,7 +8,17 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 
+//https://github.com/elidill653/RC4-Stream-Cipher/blob/master/RC4%20-Stream-Cipher.cpp
+
 using namespace std;
+
+string encrypt(string& input, string& password) {
+	return input;
+}
+
+string decrypt(string& input, string& password) {
+	return input;
+}
 
 void split_string(const string& received, string &username, string& message) {
 	for (size_t i = 0; i < received.length(); i++) {
@@ -26,11 +36,17 @@ void split_string(const string& received, string &username, string& message) {
 
 
 int main(int argc, char *argv[]) {
-    if (argc != 4) {
-        cerr << "Usage: " << argv[0] << " <server> <port> <username>" << endl;
+	
+	if (argc < 4) {
+        cerr << "Usage: " << argv[0] << " <server> <port> <username> (optional)<password>" << endl;
         return 1;
     }
-
+	string password = "";
+	
+	if (argc == 5) {
+		string password = argv[4];
+	}
+	
     string server_name = argv[1];
 	
     int port = atoi(argv[2]);
@@ -64,8 +80,22 @@ int main(int argc, char *argv[]) {
         string message_content;
         cout << "Enter your message: ";
         getline(cin, message_content);
-
-		string full_message = username + ":" + message_content;
+		char encryption_flag = '0';
+		
+		if (!message_content.empty()) {
+			if (message_content[0] == '%') {
+				encryption_flag = '1';
+				message_content = message_content.substr(1);
+			}
+		}
+		
+		if (encryption_flag == '1' && argc == 5) {
+			message_content = encrypt(message_content, password);
+		} else if (encryption_flag == '1') {
+			cerr << "ERROR: can't encrypt without password!" << endl;
+		}
+		
+		string full_message = encryption_flag + username + ":" + message_content;
 		
         int n = sendto(sock, full_message.c_str(), full_message.length(), 0, (struct sockaddr *)&server_addr, server_len);
 		
@@ -88,21 +118,29 @@ int main(int argc, char *argv[]) {
 			
 			string response = buffer;
 			
-            if (response != "END") {
-				string username, message; 
-				
-				split_string(response, username, message);
-				
-				if (username.empty()) {
-					username = "Anonymous";
-				}
-				
-				cout << "Received: " << username << ": " << message << endl;
-			}
-			
-            if (response.empty() || response == "END") {
+			if (response.empty() || response == "END") {
                 break;
 			}
+			
+			char flag = buffer[0]; //encryption flag 
+			
+			response = response.substr(1);
+			
+            string username, message;
+				
+			split_string(response, username, message);
+				
+			if (username.empty()) {
+				username = "Anonymous";
+			}
+			if (flag == '1' && argc == 5) {
+				message = decrypt(message, password);
+			} else if (flag == '1') {
+				message = "<Encrypted message>";
+			} 
+				
+			cout << "Received: " << username << ": " << message << endl;
+			
         }
     }
 
